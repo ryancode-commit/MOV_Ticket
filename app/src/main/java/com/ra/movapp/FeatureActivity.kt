34c7c1dff1de4789e82d554e1b3c3e5850568d81
ryancode-commit <ui.ardyan@gmail.com>
2023-01-25 00:07:23 +0700

    feat: penambahan fitur lagi

---
+++ app/src/main/java/com/ra/movapp/FeatureActivity.kt
@@ -0,0 +1,21 @@
+package com.ra.movapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeatureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+
+
+    }
+
+    fun addNumber(a:Int,b:Int):Int{
+        return a +  b
+    }
+
+    fun addNumber2(a:Int,b:Int):Int{
+        return a +  b
+    }
+}
